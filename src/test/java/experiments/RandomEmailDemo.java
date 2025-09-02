4bfb7e533da25c50fe33638a6d2850b5b137c2ed
Arun <arun.motoori@gmail.com>
2025-09-02 21:05:38 +0530

    TestNG Hybrid Framework - PART 2

---
+++ src/test/java/experiments/RandomEmailDemo.java
@@ -0,0 +1,12 @@
+package experiments;
+
+public class RandomEmailDemo {
+
+	public static void main(String[] args) {
+		
+		String email = "arunmotoori"+System.currentTimeMillis()+"@gmail.com";
+		System.out.println(email);
+
+	}
+
+}
---
+++ src/test/java/tests/Login.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test(priority=1)
+	public void verifyLoginWithValidCredentias() {
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void verifyLoginWithInvalidCredentials() {
+		
+		
+	}
+	
+	@Test(priority=3)
+	public void verifyLoginWithInvalidEmailAndValidPassword() {
+		
+		
+	}
+	
+	@Test
+	public void verifyLoginWithValidEmailAndInvalidPassword() {
+		
+		
+	}
+	
+	@Test
+	public void verifyLoginWithoutEnteringAnyCredentials() {
+		
+		
+	}
+
+}
